f2893bb207e002c22a32fac00a636c22d7f8acfb
Sébastien M. P <sebastien.popoff@espci.fr>
2019-05-18 13:09:21 +0200

    Add files via upload
    Initial commit

---
+++ complexFunctions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 19:35:11 2019
+
+@author: spopoff
+"""
+
+
+from torch.nn.functional import relu, max_pool2d
+
+#%%
+
+
+
+def complex_relu(input_r,input_i):
+#    assert(input_r.size() == input_i.size())
+    return relu(input_r), relu(input_i)
+
+def complex_max_pool(input_r,input_i,kernel_size, stride, padding,
+                                dilation, ceil_mode, return_indices):
+    return max_pool2d(input_r, kernel_size, stride, padding, dilation, 
+                      ceil_mode, return_indices), \
+           max_pool2d(input_i, kernel_size, stride, padding, dilation, 
+                      ceil_mode, return_indices)
