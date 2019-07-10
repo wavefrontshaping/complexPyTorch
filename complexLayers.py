dbacb2c76cdc188894dde77cd46bb13074f66185
Sebastien Popoff <spopoff@fermat.institut-langevin.local>
2019-07-10 15:36:06 +0200

    bug

--- complexLayers.py
+++ complexLayers.py
@@ -40,7 +40,7 @@ class ComplexDropout2d(Module):
         return complex_dropout2d(input_r,input_i,self.p,self.inplace)
 
 class ComplexMaxPool2d(Module):
-    
+
     def __init__(self,kernel_size, stride= None, padding = 0,
                  dilation = 1, return_indices = False, ceil_mode = False):
         super(ComplexMaxPool2d,self).__init__()
@@ -52,13 +52,13 @@ class ComplexMaxPool2d(Module):
         self.return_indices = return_indices
 
     def forward(self,input_r,input_i):
-        return complex_max_pool2d(input_r,input_i,kernel_size = self.kernel_size, 
+        return complex_max_pool2d(input_r,input_i,kernel_size = self.kernel_size,
                                 stride = self.stride, padding = self.padding,
                                 dilation = self.dilation, ceil_mode = self.ceil_mode,
                                 return_indices = self.return_indices)
-     
-class ComplexRelu(Module):
-    
+
+class ComplexReLU(Module):
+
      def forward(self,input_r,input_i):
          return complex_relu(input_r,input_i)
 
@@ -75,8 +75,8 @@ class ComplexConv2d(Module):
 #        assert(input_r.size() == input_i.size())
         return self.conv_r(input_r)-self.conv_i(input_i), \
                self.conv_r(input_i)+self.conv_i(input_r)
- 
-    
+
+
 class ComplexLinear(Module):
 
     def __init__(self, in_features, out_features):
@@ -99,8 +99,8 @@ class NaiveComplexBatchNorm1d(Module):
         self.bn_i = BatchNorm1d(num_features, eps, momentum, affine, track_running_stats)
 
     def forward(self,input_r, input_i):
-        return self.bn_r(input_r), self.bn_i(input_i)    
-   
+        return self.bn_r(input_r), self.bn_i(input_i)
+
 class NaiveComplexBatchNorm2d(Module):
     '''
     Naive approach to complex batch norm, perform batch norm independently on real and imaginary part.
@@ -112,7 +112,7 @@ class NaiveComplexBatchNorm2d(Module):
         self.bn_i = BatchNorm2d(num_features, eps, momentum, affine, track_running_stats)
 
     def forward(self,input_r, input_i):
-        return self.bn_r(input_r), self.bn_i(input_i)    
+        return self.bn_r(input_r), self.bn_i(input_i)
 
 class NaiveComplexBatchNorm1d(Module):
     '''
@@ -125,8 +125,8 @@ class NaiveComplexBatchNorm1d(Module):
         self.bn_i = BatchNorm1d(num_features, eps, momentum, affine, track_running_stats)
 
     def forward(self,input_r, input_i):
-        return self.bn_r(input_r), self.bn_i(input_i)   
-    
+        return self.bn_r(input_r), self.bn_i(input_i)
+
 class _ComplexBatchNorm(Module):
 
     def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True,
@@ -153,8 +153,8 @@ class _ComplexBatchNorm(Module):
             self.register_parameter('running_mean', None)
             self.register_parameter('running_covar', None)
             self.register_parameter('num_batches_tracked', None)
-        self.reset_parameters()    
-        
+        self.reset_parameters()
+
     def reset_running_stats(self):
         if self.track_running_stats:
             self.running_mean.zero_()
@@ -169,14 +169,14 @@ class _ComplexBatchNorm(Module):
             init.constant_(self.weight[:,:2],1.4142135623730951)
             init.zeros_(self.weight[:,2])
             init.zeros_(self.bias)
-   
-class ComplexBatchNorm2d(_ComplexBatchNorm):     
-    
+
+class ComplexBatchNorm2d(_ComplexBatchNorm):
+
     def forward(self, input_r, input_i):
         assert(input_r.size() == input_i.size())
         assert(len(input_r.shape) == 4)
         exponential_average_factor = 0.0
-        
+
 
         if self.training and self.track_running_stats:
             if self.num_batches_tracked is not None:
@@ -186,23 +186,23 @@ class ComplexBatchNorm2d(_ComplexBatchNorm):
                 else:  # use exponential moving average
                     exponential_average_factor = self.momentum
 
-       
+
         if self.training:
-             
+
             # calculate mean of real and imaginary part
             mean_r = input_r.mean([0, 2, 3])
             mean_i = input_i.mean([0, 2, 3])
-      
+
 
             mean = torch.stack((mean_r,mean_i),dim=1)
-            
+
             # update running mean
             self.running_mean = exponential_average_factor * mean\
                 + (1 - exponential_average_factor) * self.running_mean
-            
+
             input_r = input_r-mean_r[None, :, None, None]
-            input_i = input_i-mean_i[None, :, None, None]          
-        
+            input_i = input_i-mean_i[None, :, None, None]
+
             # Elements of the covariance matrix (biased for train)
             n = input_r.numel() / input_r.size(1)
             Crr = 1./n*input_r.pow(2).sum(dim=[0,2,3])+self.eps
@@ -211,13 +211,13 @@ class ComplexBatchNorm2d(_ComplexBatchNorm):
 
             self.running_covar[:,0] = exponential_average_factor * Crr * n / (n - 1)\
                 + (1 - exponential_average_factor) * self.running_covar[:,0]
-            
+
             self.running_covar[:,1] = exponential_average_factor * Cii * n / (n - 1)\
                 + (1 - exponential_average_factor) * self.running_covar[:,1]
-                
+
             self.running_covar[:,2] = exponential_average_factor * Cri * n / (n - 1)\
                 + (1 - exponential_average_factor) * self.running_covar[:,2]
-                
+
         else:
             mean = self.running_mean
             Crr = self.running_covar[:,0]+self.eps
@@ -226,7 +226,7 @@ class ComplexBatchNorm2d(_ComplexBatchNorm):
 
             input_r = input_r-mean[None,:,0,None,None]
             input_i = input_i-mean[None,:,1,None,None]
-                 
+
         # calculate the inverse square root the covariance matrix
         det = Crr*Cii-Cri.pow(2)
         s = torch.sqrt(det)
@@ -235,7 +235,7 @@ class ComplexBatchNorm2d(_ComplexBatchNorm):
         Rrr = (Cii + s) * inverse_st
         Rii = (Crr + s) * inverse_st
         Rri = -Cri * inverse_st
-                
+
         input_r, input_i = Rrr[None,:,None,None]*input_r+Rri[None,:,None,None]*input_i, \
                            Rii[None,:,None,None]*input_i+Rri[None,:,None,None]*input_r
 
@@ -246,17 +246,17 @@ class ComplexBatchNorm2d(_ComplexBatchNorm):
                                self.bias[None,:,1,None,None]
 
         return input_r, input_i
-     
-        
-class ComplexBatchNorm1d(_ComplexBatchNorm):     
-    
+
+
+class ComplexBatchNorm1d(_ComplexBatchNorm):
+
     def forward(self, input_r, input_i):
         assert(input_r.size() == input_i.size())
         assert(len(input_r.shape) == 2)
         #self._check_input_dim(input)
 
         exponential_average_factor = 0.0
-        
+
 
         if self.training and self.track_running_stats:
             if self.num_batches_tracked is not None:
@@ -267,21 +267,21 @@ class ComplexBatchNorm1d(_ComplexBatchNorm):
                     exponential_average_factor = self.momentum
 
         if self.training:
-             
+
             # calculate mean of real and imaginary part
             mean_r = input_r.mean(dim=0)
             mean_i = input_i.mean(dim=0)
             mean = torch.stack((mean_r,mean_i),dim=1)
-            
+
             # update running mean
             self.running_mean = exponential_average_factor * mean\
                 + (1 - exponential_average_factor) * self.running_mean
 
-            # zero mean values            
+            # zero mean values
             input_r = input_r-mean_r[None, :]
-            input_i = input_i-mean_i[None, :]          
-            
-        
+            input_i = input_i-mean_i[None, :]
+
+
             # Elements of the covariance matrix (biased for train)
             n = input_r.numel() / input_r.size(1)
             Crr = input_r.var(dim=0,unbiased=False)+self.eps
@@ -290,22 +290,22 @@ class ComplexBatchNorm1d(_ComplexBatchNorm):
 
             self.running_covar[:,0] = exponential_average_factor * Crr * n / (n - 1)\
                 + (1 - exponential_average_factor) * self.running_covar[:,0]
-            
+
             self.running_covar[:,1] = exponential_average_factor * Cii * n / (n - 1)\
                 + (1 - exponential_average_factor) * self.running_covar[:,1]
-                
+
             self.running_covar[:,2] = exponential_average_factor * Cri * n / (n - 1)\
                 + (1 - exponential_average_factor) * self.running_covar[:,2]
-                
+
         else:
             mean = self.running_mean
             Crr = self.running_covar[:,0]+self.eps
             Cii = self.running_covar[:,1]+self.eps
             Cri = self.running_covar[:,2]
-            # zero mean values 
+            # zero mean values
             input_r = input_r-mean[None,:,0]
             input_i = input_i-mean[None,:,1]
-            
+
         # calculate the inverse square root the covariance matrix
         det = Crr*Cii-Cri.pow(2)
         s = torch.sqrt(det)
@@ -314,7 +314,7 @@ class ComplexBatchNorm1d(_ComplexBatchNorm):
         Rrr = (Cii + s) * inverse_st
         Rii = (Crr + s) * inverse_st
         Rri = -Cri * inverse_st
-        
+
         input_r, input_i = Rrr[None,:]*input_r+Rri[None,:]*input_i, \
                            Rii[None,:]*input_i+Rri[None,:]*input_r
 
@@ -326,4 +326,3 @@ class ComplexBatchNorm1d(_ComplexBatchNorm):
 
         del Crr, Cri, Cii, Rrr, Rii, Rri, det, s, t
         return input_r, input_i
-
