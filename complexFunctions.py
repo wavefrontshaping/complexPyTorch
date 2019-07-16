bfbdf8efa986e5eaf026bd40be3e911edf4732f0
Sebastien Popoff <spopoff@fermat.institut-langevin.local>
2019-07-16 18:53:53 +0200

    correct dropout bug

--- complexFunctions.py
+++ complexFunctions.py
@@ -13,16 +13,16 @@ def complex_relu(input_r,input_i):
 def complex_max_pool2d(input_r,input_i,kernel_size, stride=None, padding=0,
                                 dilation=1, ceil_mode=False, return_indices=False):
 
-    return max_pool2d(input_r, kernel_size, stride, padding, dilation, 
+    return max_pool2d(input_r, kernel_size, stride, padding, dilation,
                       ceil_mode, return_indices), \
-           max_pool2d(input_i, kernel_size, stride, padding, dilation, 
+           max_pool2d(input_i, kernel_size, stride, padding, dilation,
                       ceil_mode, return_indices)
 
 def complex_dropout(input_r,input_i, p=0.5, training=True, inplace=False):
-    return complex_dropout(input_r, p, training, inplace), \
-           complex_dropout(input_r, p, training, inplace)
+    return dropout(input_r, p, training, inplace), \
+           dropout(input_i, p, training, inplace)
 
 
 def complex_dropout2d(input_r,input_i, p=0.5, training=True, inplace=False):
-    return complex_dropout2d(input_r, p, training, inplace), \
-           complex_dropout2d(input_r, p, training, inplace)
+    return dropout2d(input_r, p, training, inplace), \
+           dropout2d(input_i, p, training, inplace)
