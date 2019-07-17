431f2d670b70575f54baed3d8de966526ec269f1
Sebastien Popoff <spopoff@fermat.institut-langevin.local>
2019-07-18 00:01:00 +0200

    add ConvTranpose2d

--- complexLayers.py
+++ complexLayers.py
@@ -12,6 +12,7 @@ Based on https://openreview.net/forum?id=H1T2hmZAb
 import torch
 from torch.nn import Module, Parameter, init, Sequential
 from torch.nn import Conv2d, Linear, BatchNorm1d, BatchNorm2d
+from torch.nn import ConvTranspose2d
 from complexFunctions import complex_relu, complex_max_pool2d
 from complexFunctions import complex_dropout, complex_dropout2d
 
@@ -62,6 +63,22 @@ class ComplexReLU(Module):
      def forward(self,input_r,input_i):
          return complex_relu(input_r,input_i)
 
+class ComplexConvTranspose2d(Module):
+
+    def __init__(self,in_channels, out_channels, kernel_size, stride=1, padding=0,
+                 output_padding=0, groups=1, bias=True, dilation=1, padding_mode='zeros'):
+
+        super(ComplexConvTranspose2d, self).__init__()
+
+        self.conv_tran_r = ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding,
+                                       output_padding, groups, bias, dilation, padding_mode)
+        self.conv_tran_i = ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding,
+                                       output_padding, groups, bias, dilation, padding_mode)
+
+
+    def forward(self,input_r,input_i):
+        return self.conv_tran_r(input_r)-self.conv_tran_i(input_i), \
+               self.conv_tran_r(input_i)+self.conv_tran_i(input_r)
 
 class ComplexConv2d(Module):
 
